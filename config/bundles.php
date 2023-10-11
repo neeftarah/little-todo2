9f5f69c0bea8981716a99e814214e0e82e236ba2
Jérémy MOREAU <jmoreau.dev@gmail.com>
2023-10-11 23:57:11 +0200

    Close #19: Create template

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Pyrrah\GravatarBundle\PyrrahGravatarBundle::class => ['all' => true],
 ];
